bb7dd44cda469e415eb368facafe2b3479ba6d0b
The_Pi$ith <pisith@Pisiths-MBP.bbrouter>
2024-04-07 17:30:40 +0700

    added simple string formatter

--- Main.java
+++ Main.java
@@ -16,6 +16,13 @@ class robot {
     }
 }
 
+class MyUtils {
+    public static String moneyFormatter(long money) {
+        return String.format("I'm having around %,d in my bank account", money);
+//        return "I'm having around in my bank account";
+    }
+}
+
 class Main {
     public static void main(String[] args) {
         System.out.println("Object Wapper Demo");
@@ -35,5 +42,10 @@ class Main {
         // Auto wrapped and unwrap (int => Integer)
         int intList0 = intList.get(0);
         System.out.println("intList0 : " + intList0);
+
+        // =======
+        long money = 1000000L;
+        String bankAcc = MyUtils.moneyFormatter(money);
+        System.out.println(bankAcc);
     }
 }
